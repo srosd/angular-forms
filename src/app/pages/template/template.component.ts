e794769a0b7959bd44bee2062729c74b317a155d
Sergio <srosduran@gmail.com>
2021-01-02 11:55:57 +0100

    first commit

---
+++ src/app/pages/template/template.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styleUrls: ['./template.component.css']
+})
+export class TemplateComponent implements OnInit {
+
+  usuario = {
+    nombre: 'Sergio',
+    apellido: 'Ros',
+    correo: 'srd@srd.com'
+  }
+
+  constructor( private paisService: PaisService ) { }
+
+  ngOnInit(): void {
+    this.paisService.getPaises()
+      .subscribe(paises => {
+        console.log(paises);
+      });
+  }
+
+  guardar( forma: NgForm ){
+
+    console.log( forma );
+
+
+    if(forma.invalid){
+      
+      Object.values(forma.controls).forEach((control)=>{
+        control.markAsTouched();
+      })
+      
+      return;
+    }
+
+
+    console.log( forma.value );
+  }
+
+}
